a6a1d7d655dfab885582430ea1fb591b408f292a
master-jedii <pemakon.vlr@gmail.com>
2025-02-13 17:35:42 +0700

    query laravel form db

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,27 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
     function index(){
-        $blogs = [
-            [
-                'title' => "บทความที่ 1 ",
-                'content' => "เนื้อหาบทความที่ 1 ",
-                'status' => true
-            ],
-            [
-                'title' => "บทความที่ 2 ",
-                'content' => "เนื้อหาบทความที่ 2 ",
-                'status' => false
-            ],
-            [
-                'title' => "บทความที่ 3 ",
-                'content' => "เนื้อหาบทความที่ 3 ",
-                'status' => true
-            ]
-        ];
+        $blogs = DB::table('blogs')->get();
         return view('blog',compact('blogs'));
     }
 
